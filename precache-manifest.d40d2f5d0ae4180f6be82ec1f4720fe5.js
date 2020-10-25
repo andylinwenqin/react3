56e32c4b6bfde93b553e23c1a964e99c174c5e04
林文钦 <44943235+andylinwenqin@users.noreply.github.com>
2020-10-25 10:34:46 +0800

    Add files via upload

---
+++ precache-manifest.d40d2f5d0ae4180f6be82ec1f4720fe5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6217999eb7b8777f71f317a1fecfc258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f679c9e13ad6d41ff5c",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "af91c212040b0ecd0296",
+    "url": "/static/css/main.b0bfc7f3.chunk.css"
+  },
+  {
+    "revision": "0f679c9e13ad6d41ff5c",
+    "url": "/static/js/2.6d59a620.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.6d59a620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af91c212040b0ecd0296",
+    "url": "/static/js/main.e8462108.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "423b7358489d3e9e6eed11c5385f47d2",
+    "url": "/static/media/gouwu.423b7358.png"
+  },
+  {
+    "revision": "92a82db2299e5306053c27ec4e0f766f",
+    "url": "/static/media/kefu.92a82db2.png"
+  },
+  {
+    "revision": "9b97bcee7b8846d7633660134565b38d",
+    "url": "/static/media/left1.9b97bcee.jpg"
+  },
+  {
+    "revision": "7b2575f5bd5cf00c6bfd0bbffa6bbc4b",
+    "url": "/static/media/left2.7b2575f5.jpg"
+  },
+  {
+    "revision": "9487003d4ab0526b18347ba96035195d",
+    "url": "/static/media/left3.9487003d.jpg"
+  },
+  {
+    "revision": "c879fab879ab692c864ccae1086d6797",
+    "url": "/static/media/left4.c879fab8.jpg"
+  },
+  {
+    "revision": "914f087483321141971d6b272631d92c",
+    "url": "/static/media/left5.914f0874.jpg"
+  },
+  {
+    "revision": "f185253f73c669944f878328668013c1",
+    "url": "/static/media/lunbo1.f185253f.png"
+  },
+  {
+    "revision": "c8a1b442c776371dbf5f6d7f278bd9e7",
+    "url": "/static/media/lunbo2.c8a1b442.png"
+  },
+  {
+    "revision": "93badb24a7fcdb5b36d8a864961330d1",
+    "url": "/static/media/lunbo4.93badb24.png"
+  },
+  {
+    "revision": "fb46b6ecbf059a6de2f80107e8a1cf39",
+    "url": "/static/media/lunbo7.fb46b6ec.png"
+  },
+  {
+    "revision": "3b27aa8143cbecb5ae212eb1cd6d17e0",
+    "url": "/static/media/right1.3b27aa81.jpg"
+  },
+  {
+    "revision": "87fba612e2a82223c7911db3c42923ab",
+    "url": "/static/media/right2.87fba612.jpg"
+  },
+  {
+    "revision": "e4730b1e318e149378c9a4ecb1634738",
+    "url": "/static/media/right3.e4730b1e.jpg"
+  },
+  {
+    "revision": "2d3c28bbc1052fd2a37ec4ff062c4ef2",
+    "url": "/static/media/right4.2d3c28bb.jpg"
+  },
+  {
+    "revision": "402ccc9d7a627eec92b6d0c78f5d8f6c",
+    "url": "/static/media/right5.402ccc9d.jpg"
+  },
+  {
+    "revision": "a517b05f83ad9b96cd90440ad8ab05d3",
+    "url": "/static/media/taobao.a517b05f.png"
+  },
+  {
+    "revision": "c4876f188a89703236169093dcf44ed3",
+    "url": "/static/media/xingxing.c4876f18.png"
+  }
+]);
